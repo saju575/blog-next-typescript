d143c5b72c6067a94464c68e272b0f5a94949381
saju575 <sakilahmedsaju575@gmail.com>
2024-04-10 11:40:34 +0600

    login and register added

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const jsonServer = require("json-server");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY =
+  require("./node_modules/json-server-auth/dist/constants").JWT_SECRET_KEY;
 const auth = require("json-server-auth");
 
 const server = jsonServer.create();
@@ -17,8 +20,26 @@ server.use((req, res, next) => {
 });
 server.use(auth);
 
-server.use("/", router); // You can change the base URL here, e.g., '/api'
+server.get("/profile", auth, (req, res, next) => {
+  const token = req.header("Authorization")
+    ? req.header("Authorization").replace("Bearer ", "")
+    : null;
+  if (token) {
+    try {
+      const data = jwt.verify(token, JWT_SECRET_KEY);
 
+      const { db } = req.app;
+      let user = db.get("users").find({ email: data.email }).value();
+      res.json({ ...user, password: undefined });
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  } else {
+    res.json({ error: { name: "User not authorized" } });
+  }
+});
+
+server.use("/", router); // You can change the base URL here, e.g., '/api'
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
